6afd558fe430702b99d95315133216cc4c33dcf0
florian <florian.becker@q-two.net>
2021-09-28 12:13:19 +0200

    added listener example

--- demo/app.js
+++ demo/app.js
@@ -1,3 +1,3 @@
-const DoubleSlider = require("bootstrap-double-slider")
+const DoubleSlider = require("bootstrap-double-slider"), slider = new DoubleSlider(".bs-multi-range", 50)
 
-new DoubleSlider(".bs-multi-range", 50)
+slider.el.addEventListener("change", event => console.log(event.detail.target))
